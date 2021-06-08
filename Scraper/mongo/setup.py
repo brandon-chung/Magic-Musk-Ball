c4dea43d305215f8dfa8199b18884bb581dcde39
Brandon Chung <b.chung8612@gmail.com>
2021-06-07 22:55:19 -0700

    Fixed import error

--- Scraper/mongo/setup.py
+++ Scraper/mongo/setup.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
+import pymongo
 
 client = MongoClient()
-db = client['tweets']
-db['elonmusk'].create_index([('Tweet_Id', pymongo.ASCENDING)], unique=True)
+db = client["tweets"]
+db["elonmusk"].create_index([("Tweet_Id", pymongo.ASCENDING)], unique=True)
